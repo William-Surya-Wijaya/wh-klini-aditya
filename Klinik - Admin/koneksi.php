15da2b048128d2a33ad5449a789fe209b8b48dd5
arxent <danielchristian81@gmail.com>
2023-11-28 15:47:07 +0700

    Create koneksi.php

---
+++ Klinik - Admin/koneksi.php
@@ -0,0 +1,10 @@
+<?php
+$koneksi = mysqli_connect("williamsuryawijaya.muy.id","williams_latihan_klinik","james","williams_latihan_klinik");
+
+if(mysqli_connect_errno()){
+    echo "Koneksi database gagal : ". mysqli_connect_errno();
+}
+
+
+
+?>
